79a8eb95fd82dfb47fe01707e10b17578a781599
Shinji76 <francesco.pozza.3@gmail.com>
2023-02-05 12:42:35 +0100

    json file sistemati

--- PROGETTO QT/Progetto/Converter/Json.h
+++ PROGETTO QT/Progetto/Converter/Json.h
@@ -1,8 +1,8 @@
-#ifndef CONVERTER_JSON_H
-#define CONVERTER_JSON_H
+#ifndef JSON_H
+#define JSON_H
 
 #include <QJsonObject>
-#include "../AbstractCard.h"
+#include "../Cards/AbstractCard.h"
 #include "IReader.h"
 
 class Json {
@@ -12,8 +12,8 @@ private:
 public:
 	Json(IReader& reader);
 	const IReader& getReader() const;
-	QJsonObject fromObject(const AbstractCard& item) const;
+	QJsonObject fromObject(const AbstractCard& card) const;
 	AbstractCard* toObject(const QJsonObject& json) const;
 };
 
-#endif //CONVERTE_JSON_H
+#endif //JSON_H
