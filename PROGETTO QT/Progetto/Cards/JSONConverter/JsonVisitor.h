b8694c061392fbf31349a972ea5d9116392d52d4
Shinji76 <francesco.pozza.3@gmail.com>
2023-02-05 14:15:08 +0100

    jsonvisitor.h finito

---
+++ PROGETTO QT/Progetto/Cards/JSONConverter/JsonVisitor.h
@@ -0,0 +1,22 @@
+#ifndef JSON_VISITOR_H
+#define JSON_VISITOR_H
+
+#include "../IConstVisitor.h"
+#include "../Mostro.h"
+#include "../Magia.h"
+#include "../Segreto.h"
+#include <QJsonObject>
+
+
+class JsonVisitor: public IConstVisitor {
+private:
+    QJsonObject object;
+
+public:
+    QJsonObject getObject() const;
+    void visit(const Mostro& mostro);
+    void visit(const Magia& magia);
+    void visit(const Segreto& segreto);
+};
+
+#endif  //JSON_VISITOR_H
