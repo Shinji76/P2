1bc8ce4c28539f64e60ef1dcb2f4f95414122e90
Shinji76 <francesco.pozza.3@gmail.com>
2023-02-05 14:09:21 +0100

    json.h finito

---
+++ PROGETTO QT/Progetto/Cards/JSONConverter/Json.h
@@ -0,0 +1,18 @@
+#ifndef JSON_H
+#define JSON_H
+
+#include "IReader.h"
+#include "../AbstractCard.h"
+#include <QJsonObject>
+class Json {
+private:
+	IReader& reader;
+
+public:
+	Json(IReader& reader);
+	const IReader& getReader() const;
+	QJsonObject fromObject(const AbstractCard& card) const;
+	AbstractCard* toObject(const QJsonObject& json) const;
+};
+
+#endif //JSON_H
