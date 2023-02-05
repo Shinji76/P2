c5961b9eab68b5d302f7dae3d32c10e20ab39c08
Shinji76 <francesco.pozza.3@gmail.com>
2023-02-05 12:43:41 +0100

    sistemare gestione eccezioni

--- PROGETTO QT/Progetto/Converter/Reader.cpp
+++ PROGETTO QT/Progetto/Converter/Reader.cpp
@@ -1,7 +1,7 @@
+#include "../Cards/Mostro.h"
+#include "../Cards/Magia.h"
+#include "../Cards/Segreto.h"
 #include "Reader.h"
-#include "../Mostro.h"
-#include "../Magia.h"
-#include "../Segreto.h"
 
 #include <stdexcept>
 //#include <QJsonArray>
@@ -17,23 +17,23 @@ Reader& Reader::clear() {
 }
 
 AbstractCard* Reader::read(const QJsonObject& object) {
-    QJsonValue type = object.value("type");
-    if (type.isUndefined()) {
-        Service::Logger::Singleton::get().error("Missing item type.");
-        throw std::invalid_argument("Missing item type.");
-    }
+    QJsonValue type = object.value("Type");
+    //if (type.isUndefined()) {
+    //    Service::Logger::Singleton::get().error("Missing item type.");
+    //    throw std::invalid_argument("Missing item type.");
+    //}
 
     const unsigned int identifier = object.value("id").toInt();
     if (cache.count(identifier) > 0) {
         return cache[identifier];
     }
-    else if (type.toString().compare("mostro") == 0) {
+    else if (type.toString().compare("Mostro") == 0) {
         cache[identifier] = readMostro(object);
     }
-    else if (type.toString().compare("magia") == 0) {
+    else if (type.toString().compare("Magia") == 0) {
         cache[identifier] = readMagia(object);
     }
-    else if (type.toString().compare("segreto") == 0) {
+    else if (type.toString().compare("Segreto") == 0) {
         cache[identifier] = readSegreto(object);
     }
     return cache[identifier];
@@ -42,35 +42,36 @@ AbstractCard* Reader::read(const QJsonObject& object) {
 AbstractCard* Reader::readMostro(const QJsonObject& object) const {
     return new Mostro(
         object.value("ID").toUnsignedInt(),
-        object.value("nome").toString().toStdString(),
-        object.value("effetto").toString().toStdString(),
-        object.value("mana").toUnsignedInt(),
-        object.value("rarita").toString().toStdString(),    //da sistemare con enum
-        object.value("classe").toString().toStdString(),    //da sistemare con enum
-        object.value("attacco").toUnsignedInt(),
-        object.value("difesa").toUnsignedInt(),
+        object.value("Nome").toString().toStdString(),
+        object.value("Effetto").toString().toStdString(),
+        object.value("Mana").toUnsignedInt(),
+        object.value("Rarita").toString().toStdString(),    //da sistemare con enum
+        object.value("Classe").toString().toStdString(),    //da sistemare con enum
+        object.value("Razza").toString().toStdString(),    //da sistemare con enum
+        object.value("Attacco").toUnsignedInt(),
+        object.value("Difesa").toUnsignedInt(),
     );
 }
 
 AbstractCard* Reader::readMagia(const QJsonObject& object) const {
     return new Magia(
         object.value("ID").toUnsignedInt(),
-        object.value("nome").toString().toStdString(),
-        object.value("effetto").toString().toStdString(),
-        object.value("mana").toUnsignedInt(),
+        object.value("Nome").toString().toStdString(),
+        object.value("Effetto").toString().toStdString(),
+        object.value("Mana").toUnsignedInt(),
         object.value("rarita").toString().toStdString(),    //da sistemare con enum
-        object.value("classe").toString().toStdString(),    //da sistemare con enum
+        object.value("Classe").toString().toStdString(),    //da sistemare con enum
     );
 }
 
 AbstractCard* Reader::readSegreto(const QJsonObject& object) const {
     return new Segreto(
         object.value("ID").toUnsignedInt(),
-        object.value("nome").toString().toStdString(),
-        object.value("effetto").toString().toStdString(),
-        object.value("mana").toUnsignedInt(),
+        object.value("Nome").toString().toStdString(),
+        object.value("Effetto").toString().toStdString(),
+        object.value("Mana").toUnsignedInt(),
         object.value("rarita").toString().toStdString(),    //da sistemare con enum
-        object.value("classe").toString().toStdString(),    //da sistemare con enum
+        object.value("Classe").toString().toStdString(),    //da sistemare con enum
     );
 }
 
