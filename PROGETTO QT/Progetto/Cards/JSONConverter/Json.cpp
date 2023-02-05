a656d10472c84a1d044163dedf7c336e3f5048af
Shinji76 <francesco.pozza.3@gmail.com>
2023-02-05 14:07:48 +0100

    Json.cpp finito

---
+++ PROGETTO QT/Progetto/Cards/JSONConverter/Json.cpp
@@ -0,0 +1,19 @@
+#include "Json.h"
+#include "JsonVisitor.h"
+#include "../AbstractCard.h"
+
+Json::Json(IReader& reader) : reader(reader) {}
+
+const IReader& Json::getReader() const {
+    return reader;
+}
+
+QJsonObject Json::fromObject(const AbstractCard& card) const {
+    JsonVisitor json_visitor;
+    card.accept(json_visitor);
+    return json_visitor.getObject();
+}
+
+AbstractCard* Json::toObject(const QJsonObject& json) const {
+    return reader.read(json);
+}
