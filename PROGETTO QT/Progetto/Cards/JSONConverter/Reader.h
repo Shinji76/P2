2ef70068263949c0f766f590330d44dcc8ef061b
Shinji76 <francesco.pozza.3@gmail.com>
2023-02-05 14:11:09 +0100

    reader.h finito

---
+++ PROGETTO QT/Progetto/Cards/JSONConverter/Reader.h
@@ -0,0 +1,21 @@
+#ifndef JSONCONVERTER_READER_H
+#define JSONCONVERTER_READER_H
+
+#include "IReader.h"
+#include <QJsonObject>
+#include <map>
+
+class Reader: public IReader {
+private:
+	std::map<unsigned int, AbstractCard*> cache;
+	AbstractCard* readMostro(const QJsonObject& object) const;
+	AbstractCard* readMagia(const QJsonObject& object) const;
+	AbstractCard* readSegreto(const QJsonObject& object) const;
+
+public:
+	const std::map<unsigned int, AbstractCard*>& getCache() const;
+	Reader& clear();
+	virtual AbstractCard* read(const QJsonObject& object);
+};
+
+#endif //CONVERTER_JSON_READER_H
