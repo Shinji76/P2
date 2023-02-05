63aa17b8a0dc6ba2d4a64d7bd99669fbbb4ee0b6
Shinji76 <francesco.pozza.3@gmail.com>
2023-02-05 12:43:15 +0100

    header sistemato

--- PROGETTO QT/Progetto/Converter/Reader.h
+++ PROGETTO QT/Progetto/Converter/Reader.h
@@ -1,20 +1,20 @@
 #ifndef CONVERTER_JSON_READER_H
 #define CONVERTER_JSON_READER_H
 
-#include <map>
-#include <QJsonObject>
-
 #include "IReader.h"
+//#include <QJsonObject>
+
+#include <map>
 
 class Reader: public IReader {
 private:
-	std::map<unsigned int, AbstractItem*> cache;
+	std::map<unsigned int, AbstractCard*> cache;
 	AbstractCard* readMostro(const QJsonObject& object) const;
 	AbstractCard* readMagia(const QJsonObject& object) const;
 	AbstractCard* readSegreto(const QJsonObject& object) const;
 
 public:
-	const std::map<unsigned int, AbstractItem*>& getCache() const;
+	const std::map<unsigned int, AbstractCard*>& getCache() const;
 	Reader& clear();
 	virtual AbstractCard* read(const QJsonObject& object);
 };
