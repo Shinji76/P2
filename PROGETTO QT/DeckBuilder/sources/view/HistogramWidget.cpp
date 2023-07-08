1e5e066c7d763f6942c48208b48d435993099c6d
Shinji76 <francesco.pozza.3@gmail.com>
2023-07-08 18:24:24 +0200

    aggiunto histogram, da aggiustare

--- PROGETTO QT/DeckBuilder/sources/view/HistogramWidget.cpp
+++ PROGETTO QT/DeckBuilder/sources/view/HistogramWidget.cpp
@@ -1,11 +1,13 @@
 #include "HistogramWidget.h"
 
-#include <QApplication>
-#include <QtWidgets/QMainWindow>
-#include <QtCharts/QChartView>
-#include <QtCharts/QBarSet>
-#include <QtCharts/QBarSeries>
-#include <QtCharts/QLegend>
+HistogramWidget::HistogramWidget(QWidget* parent) : QWidget(parent) {
+    QHBoxLayout * hbox = new QHBoxLayout(this);
+
+    save_button = new QPushButton("Salva", this);
+    hbox->addWidget(save_button);
+    
+    delete_button = new QPushButton("Elimina", this);
+    hbox->addWidget(delete_button);
 
     QBarSet *set0 = new QBarSet("0");
     QBarSet *set1 = new QBarSet("1");
@@ -15,29 +17,6 @@
     QBarSet *set5 = new QBarSet("5");
     QBarSet *set6 = new QBarSet("6");
     QBarSet *set7 = new QBarSet("7+");
-
-    QColor *blue = new QColor(38, 118, 237);
-    QColor *black = new QColor(0, 0, 0);
-
-    set1->setColor(*blue);
-    set2->setColor(*blue);
-    set0->setColor(*blue);
-    set3->setColor(*blue);
-    set4->setColor(*blue);
-    set5->setColor(*blue);
-    set6->setColor(*blue);
-    set7->setColor(*blue);
-
-    set0->setBorderColor(*black);
-
-    *set0 << 1;
-    *set1 << 3;
-    *set2;
-    *set3 << 3;
-    *set4 << 4;
-    *set5 << 7;
-    *set6 << 2;
-    *set7 << 3;
 
     QBarSeries *series = new QBarSeries();
     series -> append(set0);
@@ -51,20 +30,35 @@
 
     QChart *chart = new QChart();
     chart->addSeries(series);
-    chart->legend()->hide();
-
-    QStringList mana_costs;
-    mana_costs << "0" << "1" << "2" << "3" << "4" << "5" << "6" << "7+";
-
-    QPalette pal = qApp -> palette();
-    pal.setColor(QPalette::Window, QRgb(0x858585));
-    pal.setColor(QPalette::WindowText, QRgb(0x505050));
-    qApp -> setPalette(pal);
+    //chart->legend()->hide();
 
     QChartView *chartView = new QChartView(chart);
-    chartView ->setRenderHint(QPainter::Antialiasing);
+    chartView->setRenderHint(QPainter::Antialiasing);
 
-    window.setCentralWidget(chartView);
-    window.resize(420, 300);
-    window.show();
-    return a.exec();
+    // Create the tooltip label
+    QLabel *tooltip = new QLabel(chartView);
+    tooltip->setFixedSize(20, 20);
+    tooltip->setAlignment(Qt::AlignCenter);
+    tooltip->setStyleSheet("QLabel {font : 15; font : bold; color : gold;}");
+
+    QObject::connect(barSet, &QBarSet::hovered, [=](bool hovered, int index) {
+        if (hovered) {
+            tooltip->setText(QString::number(barSet->at(index)));
+            qreal x = chart->plotArea().x() + chart->plotArea().width() * ((qreal)index + 0.5) / barSet->count();
+            qreal y = chart->plotArea().y() + chart->plotArea().height();
+            tooltip->move(x - tooltip->width() / 2, y - tooltip->height() * 1.2);
+            tooltip->show();
+        } else {
+            tooltip->hide();
+        }
+    }
+    );
+}
+
+void HistogramWidget::SaveClick() {
+
+}
+
+void HistogramWidget::DeleteClick() {
+
+}
