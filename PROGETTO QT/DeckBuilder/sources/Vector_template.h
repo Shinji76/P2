d9d505fe26945775298e59b268bee5eb39be678f
Shinji76 <francesco.pozza.3@gmail.com>
2023-07-08 18:23:49 +0200

    modificato vettore aggiunti overloading

--- PROGETTO QT/DeckBuilder/sources/Vector_template.h
+++ PROGETTO QT/DeckBuilder/sources/Vector_template.h
@@ -1,18 +1,22 @@
 #ifndef F_VECTOR_H
 #define F_VECTOR_H
+
+#include <exception>
+
 #include <iostream>
 
-template <class T, int N>
+template <class T>
 class FixedVector {
 private:
-    T data[N];
-    int size;
+    static const unsigned int SIZE = 50;
+    int capacity;
+    T data[SIZE];
 public:
     FixedVector();
     ~FixedVector();
     
     int getSize() const;
-    int capacity() const;
+    int getCapacity() const;
     T& operator[](int index);
     const T& operator[](int index) const;
     void push_back(const T& value);
@@ -37,7 +41,7 @@ public:
     public:
         constIterator(const T* ptr) : ptr(ptr) {}
 
-        const T& operator*();
+        const T& operator*() const;
         constIterator& operator++();	
 		constIterator operator++(int);
         bool operator!=(const constIterator& compare) const;
@@ -49,122 +53,123 @@ public:
     Iterator end();
 };
 
-template<class T, int N>
-FixedVector<T,N>::FixedVector() : size(N) {
-    for(int i = 0; i < N; ++i) {
+template<class T>
+FixedVector<T>::FixedVector() : capacity(0) {
+    for(int i = 0; i < SIZE; ++i) {
         data[i] = 0;
     }
 }
 
-template<class T, int N>
-FixedVector<T,N>::~FixedVector() {
+template<class T>
+FixedVector<T>::~FixedVector() {
     if (data) {
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < SIZE; i++) {
             data[i].~T(); 
         }
         delete(data);
     }
 }
 
-template<class T, int N>
-int FixedVector<T,N>::getSize() const {
-    return size;
+template<class T>
+int FixedVector<T>::getSize() const {
+    return SIZE;
 }
 
-template<class T, int N>
-int FixedVector<T,N>::capacity() const {
-    return N;
+template<class T>
+int FixedVector<T>::getCapacity() const {
+    return capacity;
 }
 
-template<class T, int N>
-T& FixedVector<T,N>::operator[](int index) {
+template<class T>
+T& FixedVector<T>::operator[](int index) {
     return data[index];
 }
 
-template<class T, int N>
-const T& FixedVector<T,N>::operator[](int index) const {
+template<class T>
+const T& FixedVector<T>::operator[](int index) const {
     return data[index];
 }
 
-template<class T, int N>
-void FixedVector<T,N>::push_back(const T& value) {
-    if (size < N) {
-        data[size++] = value;
+template<class T>
+void FixedVector<T>::push_back(const T& value) {
+    if (capacity < SIZE) {
+        data[capacity++] = value;
+        capacity++;
     } else {
-        //qualche errore
+        throw std::out_of_range("exceeded vector capacity");
     }
 }
 
-template<class T, int N>
-T& FixedVector<T,N>::operator=(const T& copy) {
-    for(int i = 0; i < size; i++) {
+template<class T>
+T& FixedVector<T>::operator=(const T& copy) {
+    for(int i = 0; i < SIZE; i++) {
         data[i] = copy[i];
     }
 }
 
-template<class T, int N>
-T& FixedVector<T,N>::Iterator::operator*() {
+template<class T>
+T& FixedVector<T>::Iterator::operator*() {
     return *ptr;
 }
 
-template<class T, int N>
-typename FixedVector<T,N>::Iterator& FixedVector<T,N>::Iterator::operator++() {
+template<class T>
+typename FixedVector<T>::Iterator& FixedVector<T>::Iterator::operator++() {
     ptr++;
     return *this;
 }
 
-template<class T, int N>
-typename FixedVector<T,N>::Iterator FixedVector<T,N>::Iterator::operator++(int) {
+template<class T>
+typename FixedVector<T>::Iterator FixedVector<T>::Iterator::operator++(int) {
     ptr++;
     return *this;
 }
 
-template<class T, int N>
-bool FixedVector<T,N>::Iterator::operator!=(const FixedVector<T,N>::Iterator& compare) const {
+template<class T>
+bool FixedVector<T>::Iterator::operator!=(const FixedVector<T>::Iterator& compare) const {
     return ptr != compare.ptr;
 }
 
-template<class T, int N>
-bool FixedVector<T,N>::constIterator::operator!=(const FixedVector<T,N>::constIterator& compare) const {
+template<class T>
+bool FixedVector<T>::constIterator::operator!=(const FixedVector<T>::constIterator& compare) const {
     return ptr != compare.ptr;
 }
 
-template<class T, int N>
-typename FixedVector<T,N>::Iterator FixedVector<T,N>::begin() {
+template<class T>
+typename FixedVector<T>::Iterator FixedVector<T>::begin() {
     return Iterator(data);
 }
 
-template<class T, int N>
-typename FixedVector<T,N>::Iterator FixedVector<T,N>::end() {
-    return Iterator(data + size);
+template<class T>
+typename FixedVector<T>::Iterator FixedVector<T>::end() {
+    return Iterator(data + SIZE);
 }
 
 
-template<class T, int N>
-const T& FixedVector<T,N>::constIterator::operator*() {
-    return *ptr;
+template<class T>
+const T& FixedVector<T>::constIterator::operator*() const {
+    return *ptr; 
 }
 
-template<class T, int N>
-typename FixedVector<T,N>::constIterator& FixedVector<T,N>::constIterator::operator++() {
+template<class T>
+typename FixedVector<T>::constIterator& FixedVector<T>::constIterator::operator++() {
     ptr++;
     return *this;
 }
 
-template<class T, int N>
-typename FixedVector<T,N>::constIterator FixedVector<T,N>::constIterator::operator++(int) {
+template<class T>
+typename FixedVector<T>::constIterator FixedVector<T>::constIterator::operator++(int) {
     ptr++;
     return *this;
 }
 
-template<class T, int N>
-typename FixedVector<T,N>::constIterator FixedVector<T,N>::begin() const {
+template<class T>
+typename FixedVector<T>::constIterator FixedVector<T>::begin() const {
     return constIterator(data);
 }
 
-template<class T, int N>
-typename FixedVector<T,N>::constIterator FixedVector<T,N>::end() const {
-    return constIterator(data + size);
+template<class T>
+typename FixedVector<T>::constIterator FixedVector<T>::end() const {
+    return constIterator(data + SIZE);
 }
 
 #endif //F_VECTOR_H
